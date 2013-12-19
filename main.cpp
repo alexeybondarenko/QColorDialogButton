53c29d4e16505987a2479538e3a89bfe41fb24d9
tilast <tilast.lab@gmail.com>
2013-12-19 13:02:42 +0200

    added msg

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QDebyg>
+
 int main(int argc, char *argv[])
 {
+	std::cout << 'dslfkdsj';
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
