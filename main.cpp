f6ca14aaf388ff5938f11bd59752fd2fe8738259
Alexey Bondarenko <alexeybondarenko@me.com>
2013-12-19 13:04:00 +0200

    Merges

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
+    //some comment
+    qDebug() <<"Merge test";
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
